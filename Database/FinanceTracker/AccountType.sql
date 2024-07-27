6eff8062bcf6cfd64d83466f7dd28ddf1e8bdc26
David Hayes <david.hayes@spindriftpages.net>
2024-07-27 16:16:38 -0700

    Working on DB Schema

---
+++ Database/FinanceTracker/AccountType.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[AccountType]
+(
+  [Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+  [Type] NVARCHAR(50) NOT NULL,
+)
---
+++ Database/FinanceTracker/TransactionType.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[TransactionType]
+(
+  [Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+  [Type] NVARCHAR(50) NOT NULL
+)
