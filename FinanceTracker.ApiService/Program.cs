464ea1350eda01bc7907acbc6208fd051d704837
David Hayes <david.hayes@spindriftpages.net>
2024-11-14 06:34:36 -0800

    Added some endpoints

--- FinanceTracker.ApiService/Program.cs
+++ FinanceTracker.ApiService/Program.cs
@@ -19,15 +19,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-app.MapGet("/weatherforecast", (FinanceTackerDbContext context) => { return context.AccountTypes.ToList(); });
+app.MapGet("/accountTypes", (FinanceTackerDbContext context) => { return context.AccountTypes.ToList(); });
 
 app.MapGet("/accounts", (FinanceTackerDbContext context) => { return context.Accounts.ToList(); });
 
+app.MapGet("/accounts/{id}", async (FinanceTackerDbContext context, int id) =>
+{
+    var account = await context.Accounts.FindAsync(id);
+    return account is not null ? Results.Ok(account) : Results.NotFound();
+});
+app.MapPost("/accounts", async (FinanceTackerDbContext context, Account account) =>
+{
+    context.Accounts.Add(account);
+    await context.SaveChangesAsync();
+    return Results.Created($"/accounts/{account.Id}", account);
+});
 app.MapDefaultEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
